521e0df5bcbf52944b4f96e4738952a22a9f91df
Enes Zengin <enzengin@gmail.com>
2022-01-01 20:40:43 +0300

    v1

--- FileTool.py
+++ FileTool.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 
 class crudCSV:
-
     def __init__(self,path,fields = None):
         if fields is not None: #Yeni bir CSV oluşturacaksak kullanılacak constructor
             self.path = path
@@ -11,24 +10,20 @@ class crudCSV:
         if fields is None: #Varolan CSV üzerinden işlem yapılacaksa kullanılacak constructor
             self.path = path
             self.getCSV()
-
     def createCSV(self,fields):
         df = pd.DataFrame(columns=fields)
         df.to_csv(self.path, sep=";", encoding='utf-8', columns=fields, header=fields, index=False)
         print(self.path, " dizinine csv oluşturuldu")
-
     def getCSV(self):
         df = pd.read_csv(self.path,sep = ";")
         print(df)
         return df
-
     def addData(self,list): #Column sayısı kadar ve aynı sırayla veri tutan bir lsite(tek rowa karşılık gelecek)
         df = self.getCSV()
         addRow = pd.Series(list, index=df.columns)
         df = df.append(addRow,ignore_index = True)
         df.to_csv(self.path, sep=";", encoding='utf-8', index=False)
         print(df)
-
     def deleteData(self,index):
         df = self.getCSV()
         df = df.drop(index)
